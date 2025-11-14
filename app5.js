4e5265121ccaf48582691db68b61a5e29e6fadaf
Yonehara Wataru <s24g1142tt@s.chibakoudai.jp>
2025-11-14 12:16:24 +0900

    keiyo2に駅名追加機能を追加

--- app5.js
+++ app5.js
@@ -115,16 +115,6 @@ let station = [
   { id:6, code:"JE05", name:"新浦安駅"},
 ];
 
-let station2 = [
-  { id:1, code:"JE01", name:"東京駅", change:"総武本線，中央線，etc", passengers:403831, distance:0 },
-  { id:2, code:"JE02", name:"八丁堀駅", change:"日比谷線", passengers:31071, distance:1.2 },
-  { id:3, code:"JE05", name:"新木場駅", change:"有楽町線，りんかい線", passengers:67206, distance:7.4 },
-  { id:4, code:"JE07", name:"舞浜駅", change:"舞浜リゾートライン", passengers:76156,distance:12.7 },
-  { id:5, code:"JE12", name:"新習志野駅", change:"", passengers:11655, distance:28.3 },
-  { id:6, code:"JE17", name:"千葉みなと駅", change:"千葉都市モノレール", passengers:16602, distance:39.0 },
-  { id:7, code:"JE18", name:"蘇我駅", change:"内房線，外房線", passengers:31328, distance:43.0 },
-];
-
 app.get("/keiyo", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
   res.render('db1', { data: station });
@@ -141,12 +131,29 @@ app.get("/keiyo_add", (req, res) => {
   let name = req.query.name;
   let newdata = { id: id, code: code, name: name };
   station.push( newdata );
-  res.redirect('/public/keiyo_add.html');
+  //res.redirect('/public/keiyo_add.html');
   res.render('db1', { data: station });
 });
 
+let station2 = [
+  { id:1, code:"JE01", name:"東京駅", change:"総武本線，中央線，etc", passengers:403831, distance:0 },
+  { id:2, code:"JE02", name:"八丁堀駅", change:"日比谷線", passengers:31071, distance:1.2 },
+  { id:3, code:"JE05", name:"新木場駅", change:"有楽町線，りんかい線", passengers:67206, distance:7.4 },
+  { id:4, code:"JE07", name:"舞浜駅", change:"舞浜リゾートライン", passengers:76156,distance:12.7 },
+  { id:5, code:"JE12", name:"新習志野駅", change:"", passengers:11655, distance:28.3 },
+  { id:6, code:"JE17", name:"千葉みなと駅", change:"千葉都市モノレール", passengers:16602, distance:39.0 },
+  { id:7, code:"JE18", name:"蘇我駅", change:"内房線，外房線", passengers:31328, distance:43.0 },
+];
 app.get("/keiyo2", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
+  let id = req.query.id;
+  let code = req.query.code;
+  let name = req.query.name;
+  let change = req.query.change;
+  let passengers = req.query.passengers;
+  let distance = req.query.distance;
+  let newdata = { id: id, code: code, name: name, change: change, passengers: passengers, distance: distance };
+  station2.push( newdata );
   res.render('keiyo2', {data: station2} );
 });
 
